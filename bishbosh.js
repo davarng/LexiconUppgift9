bcba84a1bfc783209066da7776c18b5b5146b3a2
davarng <113583854+davarng@users.noreply.github.com>
2025-05-08 13:43:43 +0200

    Form complete with result

--- bishbosh.js
+++ bishbosh.js
@@ -1,27 +1,35 @@
-let loop;
-
-let bish;
-
-let bosh;
-
-function BishBosh() {
+function BishBosh(loop, bish, bosh) {
+  let result = "";
   if (loop > 0 && bish > 0 && bosh > 0) {
     for (let i = 1; i <= loop; i++) {
       if (i % bish === 0 && i % bosh === 0) {
-        console.log("Bish-Bosh");
+        result += "Bish-Bosh ";
       } else if (i % bish === 0) {
-        console.log("Bish");
+        result += "Bish ";
       } else if (i % bosh === 0) {
-        console.log("Bosh");
+        result += "Bosh ";
       } else {
-        console.log(i);
+        result += `${i} `;
       }
     }
+    return result.slice(0, -1);
+  } else {
+    return "Wrong input";
   }
 }
 
 function PrintBishBosh(message) {
-  document.getElementById("message").textContent = message;
+  document.getElementById("result").textContent = message;
 }
 
-PrintBishBosh("yes");
+document
+  .getElementById("bishBoshForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const iterations = document.querySelector('[name="iterations"]').value;
+    const bish = document.querySelector('[name="bish"]').value;
+    const bosh = document.querySelector('[name="bosh"]').value;
+
+    PrintBishBosh(BishBosh(iterations, bish, bosh));
+  });
