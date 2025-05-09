d4999b686b1dc011724488f9b2fbbb1e5be3a0a0
davarng <113583854+davarng@users.noreply.github.com>
2025-05-09 15:30:43 +0200

    Finished todo

---
+++ todo.js
@@ -0,0 +1,28 @@
+function PrintBishBosh(message) {
+  document.getElementById("result").textContent = message;
+}
+
+document
+  .getElementById("todoForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const message = document.querySelector('[name="todoMessage"]').value;
+    document.getElementById("todoList").innerHTML += `
+      <li class="todo-li p-2 rounded mb-2">
+        <p class="todo-text text-wrap mb-1" style="word-wrap: break-word;">${message}</p>
+        <button class="check-btn">Check</button>
+        <button class="delete-btn">delete</button>
+      </li>
+    `;
+
+    event.target.reset();
+  });
+
+document.getElementById("todoList").addEventListener("click", function (event) {
+  if (event.target.classList.contains("delete-btn")) {
+    event.target.parentElement.remove();
+  } else if (event.target.classList.contains("check-btn")) {
+    event.target.previousElementSibling.classList.toggle("done");
+  }
+});
